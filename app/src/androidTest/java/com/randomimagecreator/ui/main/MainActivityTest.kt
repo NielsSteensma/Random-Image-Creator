530aecb1f20deaa1ddc0d7747f2dbf3df6967c6c
Niels Steensma <nielssteensma@yahoo.nl>
2021-10-16 15:51:52 +0200

    Retrigger Travis

---
+++ app/src/androidTest/java/com/randomimagecreator/ui/main/MainActivityTest.kt
@@ -0,0 +1,34 @@
+package com.randomimagecreator.ui.main
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.randomimagecreator.R
+import com.randomimagecreator.helpers.waitForViewToBeDisplayed
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTests {
+    @get:Rule
+    var activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun imageCreation_GivenValidImageCreationOptions_CreatesImages() {
+        val amount = 5
+        val width = 5
+        val height = 5
+
+        onView(withId(R.id.image_creator_option_amount)).perform(typeText(amount.toString()), closeSoftKeyboard())
+        onView(withId(R.id.image_creator_option_width)).perform(typeText(width.toString()), closeSoftKeyboard())
+        onView(withId(R.id.image_creator_option_height)).perform(typeText(height.toString()), closeSoftKeyboard())
+        onView(withId(R.id.image_creator_button_create)).perform(click())
+
+        withId(R.id.image_creator_button_create).matches(isDisplayed())
+// waitForViewToBeDisplayed(withId(R.id.recyclerview_created_images))
+    }
+}
