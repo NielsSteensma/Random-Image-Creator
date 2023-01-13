3d317370de70b3beda8852342c7edd17f3dfc4ad
Niels Steensma <nielssteensma@yahoo.nl>
2023-01-13 22:11:32 +0100

    Change decrypt_google_services.sh paths

--- bin/decrypt_google_services.sh
+++ bin/decrypt_google_services.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICES_PASSPHRASE" \
---output $HOME/app/google_services.json $HOME/app/google_services.json.gpg
+--output ../app/google_services.json ../app/google_services.json.gpg
