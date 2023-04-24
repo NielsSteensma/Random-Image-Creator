aad1228e5520cb0be8927b43c397839cbb4a6e68
Niels Steensma <nielssteensma@yahoo.nl>
2023-04-24 23:21:44 +0200

    Bugfix: Possible exception when pasting in input fields (#19)
    * Improve parsing of input fields to int
    
    * Update CreateImagesFragment.kt with changes. Disable pasting in textinput

--- app/src/main/java/com/randomimagecreator/helpers/IntExtensions.kt
+++ app/src/main/java/com/randomimagecreator/helpers/IntExtensions.kt
@@ -4,18 +4,6 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 /**
- * Converts a [CharSequence] to an [Int],
- * in case the given [CharSequence] is null or empty, 0 is returned.
- *
- * @param   value  The value to convert to an [Int].
- * @return The result of the conversion.
- */
-fun Int.Companion.parse(value: CharSequence?): Int {
-    val safeValue = if (value == null || value.isBlank()) "0" else value.toString()
-    return Integer.parseInt(safeValue)
-}
-
-/**
  * Converts the current value to a [ByteArray] with an order of Little Endian.
  */
 fun Int.toLittleEndian(): ByteArray =
---
+++ app/src/test/java/com/randomimagecreator/helpers/CharSequenceExtensionsTest.kt
@@ -0,0 +1,27 @@
+package com.randomimagecreator.helpers
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class CharSequenceExtensionsTest {
+    @Test
+    fun `safeParse given an empty character sequence returns null`() {
+        assertNull("".toInt())
+    }
+
+    @Test
+    fun `safeParse given a character sequence of whitespaces returns null`() {
+        assertNull("   ".toInt())
+    }
+
+    @Test
+    fun `safeParse given a character sequence of any number returns the number`() {
+        assertEquals(7, "7".toInt())
+    }
+
+    @Test
+    fun `safeParse given an non parse-able character returns null`() {
+        assertNull("A".toInt())
+    }
+}
