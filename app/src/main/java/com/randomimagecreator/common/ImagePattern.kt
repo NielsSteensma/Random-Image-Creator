a32ab0f75397748a002061e34f269478847e3f51
Niels Steensma <nielssteensma@yahoo.nl>
2023-10-20 21:01:17 +0200

    Feature: Sierpinksi carpet (#22)

--- app/src/main/java/com/randomimagecreator/common/ImagePattern.kt
+++ app/src/main/java/com/randomimagecreator/common/ImagePattern.kt
@@ -1,15 +1,27 @@
 package com.randomimagecreator.common
 
+import com.randomimagecreator.R
 import com.randomimagecreator.creators.ImageCreator
 import com.randomimagecreator.creators.MandelbrotCreator
 import com.randomimagecreator.creators.PixelatedCreator
+import com.randomimagecreator.creators.SierpinskiCarpetCreator
 import com.randomimagecreator.creators.SolidColorCreator
 
 /**
  * Defines all possible image creation patterns.
  */
-enum class ImagePattern(val imageCreator: ImageCreator) {
-    SOLID(SolidColorCreator()),
-    PIXELATED(PixelatedCreator()),
-    MANDELBROT(MandelbrotCreator())
+enum class ImagePattern(val localizationResourceId: Int) {
+    SOLID(R.string.image_creator_solid),
+    PIXELATED(R.string.image_creator_pixelated),
+    MANDELBROT(R.string.image_creator_mandelbrot),
+    SIERPINSKI_CARPET(R.string.image_creator_sierpinski);
+
+    fun getImageCreator(): ImageCreator {
+        return when (this) {
+            SOLID -> SolidColorCreator()
+            PIXELATED -> PixelatedCreator()
+            MANDELBROT -> MandelbrotCreator()
+            SIERPINSKI_CARPET -> SierpinskiCarpetCreator()
+        }
+    }
 }
